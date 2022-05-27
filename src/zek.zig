d2c12106e08626e94353cb7c346a71baa6b6753a
Thomas Adam <thomas@xteddy.org>
2022-05-27 20:32:00 +0100

    sync: fix semantics with dev zig for ChildProcess
    The stdlib has changed the calling semantics of std.ChildProcess.init()
    such that the heap isn't allocated directly, and a ChildProcess is
    returned directly.  See:
    
    https://github.com/ziglang/zig/commit/a0a2ce92ca129d28e22c63f7bace1672c43776b5

--- src/zek.zig
+++ src/zek.zig
@@ -1304,8 +1304,7 @@ pub const UserInterface = struct {
     }
     const Errors = error{ShellFail};
     fn shell(self: *Self, comptime args: anytype) !u32 {
-        var process = try std.ChildProcess.init(&args, self.allocator);
-        defer process.deinit();
+        var process = std.ChildProcess.init(&args, self.allocator);
         const result = try process.spawnAndWait();
         switch (result) {
             Term.Exited => |n| return n,
